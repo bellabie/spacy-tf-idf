7a46805afc26f289dd58879f05973461b3c9762a
B! <github@bcavello.com>
2018-05-06 13:00:31 -0700

    Remove whitespace, punctuation, stop words
    Cleaning up the return for better preprocessing before we start doing math with this stuff.

--- tf-idf.py
+++ tf-idf.py
@@ -2,22 +2,26 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 from collections import Counter
 from spacy.lang.en.stop_words import STOP_WORDS
-#print(STOP_WORDS)
+#print(STOP_WORDS) #if you want to see what they are
 
 #Need to connect to files, Excel or csv would probably be user friendlier
 
-source = open("BestPractest.txt","r")
+source = open("goodAtStuff.txt","r")
 text = source.read()
+source.close()
+
+#Swaps line breaks for spaces
+text = text.replace("\n", " ")
 
 phrase = nlp(text)
 
-
-words = [token.lemma_ for token in phrase]
+#Returns lemmatized text without stop words, punctuation, and white space characters
+words = [token.lemma_ for token in phrase if not (token.is_stop or token.is_punct or token.is_space)]
 
 #Create a list of all the lemmatized tokens (words)
 word_freq = Counter(words)
 word_list = list(word_freq.items()) 
-word_list.sort() #list alphabetized just for fun
+word_list.sort(key=lambda tup: tup[1], reverse=True) #list sorted by frequency
 
 #Total number of lemmatized tokens in the phrase
 print(len(word_list))
@@ -30,10 +34,12 @@ while i<len(word_list):
 	pass
 
 
-#for token in test:
-#	lemmalist[token] = token.lemma_
-#	print(lemmalist)
-
 #comments = nlp(u"")
 
 # tfidf = (count of text / all the text in the document) * natural? log of (document count / documents containing that text)
+
+# NOTE: So far, I've been using:
+#* **spaCy version:** 2.0.11
+#* **Platform:** Darwin-17.3.0-x86_64-i386-64bit
+#* **Python version:** 3.6.4
+#* **Models:** en_core_web_sm
