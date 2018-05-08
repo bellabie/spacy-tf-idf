a8dbfbad252c91f857c0144108f439f281f15b52
B! <github@bcavello.com>
2018-05-08 10:21:15 -0700

    Starting the csv/tsv processing flow

--- tf-idf.py
+++ tf-idf.py
@@ -1,15 +1,26 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
-from collections import Counter
 from spacy.lang.en.stop_words import STOP_WORDS
-import csv
+#print(STOP_WORDS) #if you want to see what they are
+from collections import Counter
+import csv #Excel or csv files would probably be user friendlier
+
+nlp = spacy.load("en_core_web_sm")
 #Change to spacy/lang/lex_attrs.py was made to fix capitalization issue
 #More info here: https://github.com/explosion/spaCy/pull/1891/files
+
 nlp.vocab["s"].is_stop = True
 #Adding s to the list of stop words
-#print(STOP_WORDS) #if you want to see what they are
 
-#Excel or csv files would probably be user friendlier
+with open("tsvin.tsv", "r") as tsvin, open('tsvout.tsv', 'w') as tsvout:
+	tsvin = csv.reader(tsvin, delimiter='\t')
+	tsvout = csv.writer(tsvout)
+
+	for row in tsvin:
+		print(', '.join(row))
+		#csvout.writerows
+		pass
+
+#similarity = doc1.similarity(doc2) #could be fun
 
 file = "shouldAvoid.txt"
 print(file)
@@ -27,6 +38,7 @@ text = nlp(text)
 
 #Returns lemmatized text without stop words, punctuation, & white space characters
 words = [token.lemma_ for token in text if not (token.is_stop or token.is_punct or token.is_space)]
+# note: I added coded/coding to the irregular verb list because something weird was happening and turning those into "cod"
 
 #print(words) #Print after cleansing to debug
 
@@ -35,7 +47,7 @@ word_count = Counter(words)
 word_list = list(word_count.items()) 
 word_list.sort(key=lambda tup: tup[1], reverse=True) #list sorted by frequency
 
-#Total number of lemmatized tokens in the phrase
+#Total number of unique lemmatized tokens in the phrase
 totalunique = len(word_list)
 
 #Now we'll do some math to find the relative frequency of each word in our text
@@ -45,7 +57,7 @@ for i in range(totalunique):
 	word, count = word_list[i]
 	count = count/totalunique
 	word_freq.append((word, count))
-	print(word_freq[i])
+	#print(word_freq[i])
 	i=i+1
 	pass
 
