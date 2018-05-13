09ab483edef8e5512cb90e7cfb2145c96623fee5
B! <github@bcavello.com>
2018-05-13 01:00:04 -0700

    Counting rows and doing math w/CSV
    Using a giant for loop, I've managed to now read in individual rows from a column, perform all the good ol' frequency analysis, and now the next step will be to incorporate the inverse document frequency

--- tf-idf.py
+++ tf-idf.py
@@ -11,19 +11,8 @@ nlp = spacy.load("en_core_web_sm")
 nlp.vocab["s"].is_stop = True
 #Adding s to the list of stop words
 
-with open("tsvin.tsv", "r") as tsvin, open('tsvout.tsv', 'w') as tsvout:
-	tsvin = csv.reader(tsvin, delimiter='\t')
-	tsvout = csv.writer(tsvout)
-
-	for row in tsvin:
-		print(', '.join(row))
-		#csvout.writerows
-		pass
-
-#similarity = doc1.similarity(doc2) #could be fun
-
 file = "shouldAvoid.txt"
-print(file)
+#print(file)
 source = open(file,"r")
 text = source.read()
 source.close()
@@ -61,7 +50,52 @@ for i in range(totalunique):
 	i=i+1
 	pass
 
+with open("tsvin.tsv", "r") as tsvin, open('tsvout.tsv', 'w') as tsvout:
+	tsvin = csv.reader(tsvin, delimiter='\t')
+	docnumber = -1
+	for row in tsvin:
+		text = row[1]
+		#Swaps line breaks for spaces, also remove apostrophes & single quotes
+		text = text.replace("\n", " ").replace("'","").replace("’","")
+		#"Be" stop words weren't caught before lemmatization with the apostrophe
+		text = text.lower()
+		#spaCy is_stop was not working on words with capitalized letters
+		
+		text = nlp(text)
+		#print(text) #Print before cleansing to debug
 
+		#Returns lemmatized text without stop words, punctuation, & white space characters
+		words = [token.lemma_ for token in text if not (token.is_stop or token.is_punct or token.is_space)]
+		# note: I added coded/coding to the irregular verb list because something weird was happening and turning those into "cod"
+		#print(words) #Print after cleansing to debug
+
+		#Create a list of all the lemmatized tokens (words)
+		word_count = Counter(words)
+		word_list = list(word_count.items()) 
+		word_list.sort(key=lambda tup: tup[1], reverse=True) #list sorted by frequency
+
+		#Total number of unique lemmatized tokens in the phrase
+		totalunique = len(word_list)
+
+		#Now we'll do some math to find the relative frequency of each word in our text
+		word_freq = list()
+
+		for i in range(totalunique):
+			word, count = word_list[i]
+			count = count/totalunique
+			word_freq.append((word, count))
+			#print(word_freq[i])
+			i=i+1
+			pass
+
+		docnumber = docnumber+1
+		print(docnumber)
+		pass
+	tsvout = csv.writer(tsvout)
+
+
+
+#similarity = doc1.similarity(doc2) #could be fun
 
 
 #comments = nlp(u"")
