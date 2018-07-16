2a0de2e1829d1a9fb8cf9ab04f4fd114ae867870
B! <github@bcavello.com>
2018-07-15 21:33:53 -0700

    Added next() for row resolution & separated tsvout

--- tf-idf.py
+++ tf-idf.py
@@ -13,52 +13,63 @@ nlp.vocab["s"].is_stop = True
 # Adding s to the list of stop words
 
 # opens the target tsv with read permissions ang outputs another tsv
-with open("tsvin.tsv", "r") as tsvsource, open('tsvout.tsv', 'w') as tsvout:
-	tsvin = csv.reader(tsvsource, delimiter='\t')
-	docnumber = -1
+with open("tsvin-b.tsv", "r") as tsvsource, open('tsvout.tsv', 'w') as tsvout:
+	tsvin = csv.reader(tsvsource, delimiter='\t') # We specify the delimiter because this was written for tsv
+	tsvwriter = csv.writer(tsvout, delimiter='\t') # This will let us fill the tsvout file
+	next(tsvin)
+	docnumber = 0 # This will be the number of documents to be used in the inverse document frequency
 	for row in tsvin:
 		docnumber = docnumber + 1
 		pass
-	print(docnumber) # this is the number of documents to be used in the inverse document frequency
-	tsvsource.seek(0)
+	print(docnumber)
+
+	tsvsource.seek(0) # IMPORTANT: Start at the top of the tsv file again after having gone through the rows once
+	next(tsvin)
+	rowtracker = 0 # for debugging, records doc increments through the tsv
 	for row in tsvin:
-		text = row[1]
+		text = row[1] # for each row in tsvin, define text as the second column
 		# Swaps line breaks for spaces, also remove apostrophes & single quotes
 		text = text.replace("\n", " ").replace("'","").replace("’","")
 		# "Be" stop words weren't caught before lemmatization with the apostrophe
 		text = text.lower()
 		# spaCy is_stop was not working on words with capitalized letters
 		
-		text = nlp(text)
+		text = nlp(text) # This converts text into [some kinda spaCy thing]
 		#print(text) # Print before cleansing to debug
 
 		# Returns lemmatized text without stop words, punctuation, & white space characters
 		words = [token.lemma_ for token in text if not (token.is_stop or token.is_punct or token.is_space)]
-		# note: I added coded/coding to the irregular verb list because something weird was happening and turning those into "cod"
+		# note: I added coded/coding to the irregular verb list because something weird was happening and turning those into "cod" (may be fixed https://github.com/explosion/spaCy/issues/2257)
 		#print(words) # Print after cleansing to debug
 
 		# Create a list of all the lemmatized tokens (words)
-		word_count = Counter(words)
-		word_list = list(word_count.items()) 
-		word_list.sort(key=lambda tup: tup[1], reverse=True) #list sorted by frequency
+		word_count = Counter(words) # this is another spaCy thing, it's not just the count
+		word_list = list(word_count.items())
+		word_list.sort(key=lambda tup: tup[1], reverse=True) # list sorted by frequency
 
-		# Total number of unique lemmatized tokens in the phrase
-		totalunique = len(word_list)
+		# Total number of unique lemmatized tokens in the docs
+		totalunique = len(word_list) # This is the count for our text frequency
+
+		# Count of documents containing a given word
+		docs_with = 1 #placeholder data
 
 		# Now we'll do some math to find the relative frequency of each word in our text
 		word_freq = list()
 		for i in range(totalunique):
 			word, count = word_list[i]
-			count = count/totalunique*math.log(1/docnumber)
+			count = count/totalunique*math.log(docnumber/docs_with)
 			word_freq.append((word, count))
+			tsvwriter.writerow(word_freq[i])
 			#print(word_freq[i])
 			i=i+1
 			pass
 		pass
-	tsvwriter = csv.writer(tsvout)
-	#tsvwriter.writerow(["test","test3"*3,"testagain"]) #writes a row containing the following: test,test3test3test3,testagain
+		rowtracker = rowtracker + 1
+		tsvwriter.writerow("")
+		tsvwriter.writerow(str(rowtracker))
 
-# tfidf = (count of word / all the words in the document) * natural? log of (document count / documents containing that word)
+
+# tfidf = (count of word / all the words in this particular document) * natural log of (document count / documents containing that word)
 
 # natural log is math.log(x), if you want a different base you can use math.log(x,base)
 
